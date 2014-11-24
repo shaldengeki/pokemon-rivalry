ebf0d47a2afc3dcec0d63a71e23c2ca801fa2230
Charles Guo <shaldengeki@gmail.com>
2014-11-23 18:21:57 -0600

    generate page structure after datafiles load

--- js/main.js
+++ js/main.js
@@ -1,13 +1,44 @@
+function showBattles(battles, player) {
+  $('#battle-box').hide();
+  $('#battles ol').empty();
+
+  // append battles to list.
+  $(battles).filter(function(index, battle) {
+    return battle.winner === player;
+  }).each(function(index, battle) {
+    var battleElt = $('<li></li>').text(battle.date + ": " + battle.location);
+    $('#battles ol').append(battleElt);
+  });
+
+  if ($('#battles ol').children().length > 0) {
+    $('#battle-box').show();
+  }
+}
+
 $(document).ready(function() {
-  // count wins for each player.
   var battles = [];
   var players = [];
+
+  // TODO: show something before datafiles have loaded.
 
   $.when($.getJSON('data/players.json', function(data) {
     players = data;
   }), $.getJSON('data/battles.json', function(data) {
     battles = data;
   })).done(function() {
+    // create basic structure of the page.
+    var scoreBoxElt = $('<section></section>').attr('id', 'score-box');
+    var scoresElt = $('<div></div>').attr('id', 'scores').attr('class', 'section-inner');
+    scoresElt.append($('<ul></ul>'));
+    scoreBoxElt.append(scoresElt);
+    $('body').append(scoreBoxElt);
+
+    var battleBoxElt = $('<section></section>').attr('id', 'battle-box');
+    var battlesElt = $('<div></div>').attr('id', 'battles').attr('class', 'section-inner');
+    battlesElt.append($('<ol></ol>'));
+    battleBoxElt.append(battlesElt);
+    $('body').append(battleBoxElt);
+
     // count wins per player.
     $(battles).each(function(index, battle) {
       $(players).filter(function(index, player) {
@@ -17,7 +48,6 @@ $(document).ready(function() {
 
     // generate score list in dom.
     $(players).each(function(name, player) {
-      console.log(player);
       var playerElt = $('<li></li>').attr('class', 'player').attr('id', player.name);
 
       // color text on hover.
@@ -29,22 +59,9 @@ $(document).ready(function() {
 
       // assemble list of battles that this player has won.
       $(playerElt).click(function() {
-        $('#battle-box').hide();
-        $('#battles ol').empty();
         // get the player name from this element's ID.
         var player = $(this).attr('id');
-
-        // append battles to list.
-        $(battles).filter(function(index, battle) {
-          return battle.winner === player;
-        }).each(function(index, battle) {
-          var battleElt = $('<li></li>').text(battle.date + ": " + battle.location);
-          $('#battles ol').append(battleElt);
-        });
-
-        if ($('#battles ol').children().length > 0) {
-          $('#battle-box').show();
-        }
+        showBattles(battles, player);
       });
 
       var scoreElt = $('<div></div>').attr('class', 'score').text(player.wins);
@@ -54,6 +71,8 @@ $(document).ready(function() {
       playerElt.append(nameElt);
       $('#scores ul').append(playerElt);
     });
-
+  }, function() {
+    // one or more of the calls failed.
+    // TODO: show an error message.
   });
 });
